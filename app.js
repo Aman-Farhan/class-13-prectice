d582361a7c92c1eeb9b5a36175be7b9534ccfb5e
Aman <amanfarhan2008@gmail.com>
2025-07-25 23:30:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+// let arr = ["ali","KhIZer","mustafa"];
+// arr = arr.toUpperCase();
+// console.log(arr);
+
+
+// let sentence = ["this is a sentece"];
+
+// for (i = 0; i < sentence.length ; i++){
+    
+
+// }
+
+
+// let usserInput = [45,50,85];
+// let a = prompt("enter your marks:");
+// if (usserInput.length <= a){
+//     console.log("are you failed");
+// } else if (usserInput.length <= a){
+//     console.log("are you failed");
+// } else if (usserInput.length === a){
+//     console.log("congurate! you are passed");
+// }else{
+//     console.log("this num not available");
+// }
+
+let arr = [45,50,85,107];
+function score (arr){
+    for (i = 0 ; i < arr.length; i++){
+        let stdMarks = arr[i]
+        if(stdMarks >= 70 ){
+            document.write(stdMarks + "Pass" + "<br>");
+        } else if (stdMarks < 69){
+            document.write(stdMarks + "failed" + "<br>");
+        } else if (stdMarks > 101){
+            document.write(stdMarks + "this num is not including passing marks" + "<br>");
+        } else {
+            document.write("sfgkuagyvu");
+        }
+    }
+}
+
+score([45, 50, 85, 90, 25, 35, 45, 56, 67, 70 ,100 ,105]);
+
+
+// let message = "this product is shit. and you guys are dumb. you are all dumb";
+// let badWords = ["shit","dumb","dumb"];
+
+// for (let i = 0 ; i <message.length; i++ ){
+//     message = message.replace(badWords[i], "****");
+// }
+// console.log(message);
+
+
+
+// let message = "this product is shit. and you guys are dumb. you are all dumb";
+// let badWords = ["shit","dumb","dumb"];
+
+// for (i = 0 ; i < message.length; i++){
+//     message = message.replace(badWords[i], "****");
+// } 
+// document.write(message);
+// alert("sorry sir");
+
+
+
+
+// let para = "This is a Paragraph in which we are going to discuss about Pakistan civil Aviation"
+
+// for (i = 0 ; i < para.length ; i++){
+//     let char = para[i];
+//     if (char >= "a" && char <= "z"){
+//         console.log("this is lowercase" + char);
+//     }
+//  else if (char >= A && char <= Z){
+//     console.log("this is a capital letter"+ char);
+// } else {
+//     console.log("this is a not alphabets"+ char);
+// }
+// }
+
+
+// let para = "This is a Paragraph in which we are going to discuss about Pakistan civil Aviation";
+// document.write(para + "<br>");
+
+// for (i = 0 ; i < para.length; i++){
+//     let char = para[i];
+//     if (char >= "a" && char <= "z"){
+//          document.write("this is lowercase    "+ char + "<br>");
+//     } else if (char >= "A" && char <= "Z"){
+//         document.write("this is capital letters " + char + "<br>");
+//     } else {
+//         document.write("this is not alphabets "+ char + "<br>");
+//     }
+// }
+
+
+
+// let para = "This is a Paragraph in which we are going to discuss about Pakistan civil Aviation";
+// document.write("para" + "<br>");
+
+// for (i = 0 ; i < para.length; i++){
+//     let char = para[i];
+//     if (char >= "a" && char <= "z"){
+//         document.write("This is Small Letter" + char + "<br>");
+//     } else if (char >= "A" && char <= "Z"){
+//         document.write("This is Capital Letter" + char + "<br>");
+//     } else {
+//         document.write("This is NOT Alphabets" + char + "<br>");
+//     }
+// }
